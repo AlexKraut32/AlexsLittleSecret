8e9f4c623736b888e44af60abc8016a4dbea99e7
Safi_Amin <Safi.amini786@gmail.com>
2023-10-09 18:11:47 -0500

    added number_divisibleBy_3_5_15

---
+++ safi_codingPractice/Number_DivisibleBy_3_5_15.java
@@ -0,0 +1,26 @@
+package safi_codingPractice;
+
+public class Number_DivisibleBy_3_5_15 {
+
+
+    public static void main(String[] args) {
+
+int num = 100;
+String by15 = "", by5= "" ,by3= "";
+
+    for(int i =1 ;i<=num;i++){
+        if(i % 15 ==0 ){
+            by15 += i + " ";
+
+        }else if(i% 3==0){
+             by5 =+ i+ " ";
+        }else if(i % 5==0){
+            by3 = i+ " ";
+        }
+    }
+        System.out.println("Divisible by 15: "+ by15);
+        System.out.println("Divisible by 3: "+ by5);
+        System.out.println("Divisible by 5: "+ by3);
+    }
+    }
+
