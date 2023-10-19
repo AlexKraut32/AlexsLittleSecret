7d1ac3238e3e38b7902102908bd348b69653eca1
Safi_Amin <Safi.amini786@gmail.com>
2023-10-19 12:39:41 -0500

    made some changes in RemoveDuplicates

--- safi_codingPractice/week4/RemoveDuplicates.java
+++ safi_codingPractice/week4/RemoveDuplicates.java
@@ -12,12 +12,12 @@ public class RemoveDuplicates {
         String result = "";
 
         for (int i = 0; i <str.length() ; i++) {
-           String ch = "" + str.charAt(i); //Assigning str to new String ch
-           if(result.contains(ch.toLowerCase())){ // in condition if result has same character it will skip the next one
+           String str2 = "" + str.charAt(i); //Assigning str to new String ch
+           if(result.contains(str2.toLowerCase())){ // in condition if result has same character it will skip the next one
                continue;
 
            }
-           result +=ch;
+           result +=str2;
         }
         return result;
     }
