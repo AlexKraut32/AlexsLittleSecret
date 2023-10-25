993cb104ad14b25cefe7ee32c4ba061969335d7e
She1ter <ievgen.p.tkachenko@gmail.com>
2023-10-25 13:42:40 -0500

    Reverse created

--- Eugene_codingTasks/week05/FindTheUnique.java
+++ Eugene_codingTasks/week05/FindTheUnique.java
@@ -2,3 +2,8 @@ package Eugene_codingTasks.week05;
 
 public class FindTheUnique {
 }
+
+/*
+Write a return method that can find the unique characters
+ from the String Ex: unique("AAABBBCCCDEF") ==> "DEF";
+ */
---
+++ Eugene_codingTasks/week05/Reverse.java
@@ -0,0 +1,9 @@
+package Eugene_codingTasks.week05;
+
+public class Reverse {
+}
+
+/*
+Write a return method that can
+ reverse String Ex: Reverse("ABCD"); ==> DCBA
+ */
