e58f283fb182662c4a15c065ad0a88f5d4fdc27b
She1ter <ievgen.p.tkachenko@gmail.com>
2023-10-25 14:30:31 -0500

    reverse completed

--- Eugene_codingTasks/week05/Reverse.java
+++ Eugene_codingTasks/week05/Reverse.java
@@ -1,6 +1,16 @@
 package Eugene_codingTasks.week05;
 
 public class Reverse {
+    public static void main(String[] args) {
+        System.out.println("reverse(\"ABCD\") = " + reverse("ABCD"));
+    }
+    public static String reverse(String str){
+        String stringResult="";
+        for (int i = str.length()-1; i >= 0; i--) {
+            stringResult+=str.charAt(i);
+        }
+        return stringResult;
+    }
 }
 
 /*
