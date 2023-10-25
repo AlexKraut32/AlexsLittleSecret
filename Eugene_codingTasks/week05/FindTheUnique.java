4f005c1855c4913274f6a44ad31d967082274be1
She1ter <ievgen.p.tkachenko@gmail.com>
2023-10-25 14:00:32 -0500

    findTheUnique completed

--- Eugene_codingTasks/week05/FindTheUnique.java
+++ Eugene_codingTasks/week05/FindTheUnique.java
@@ -1,6 +1,17 @@
 package Eugene_codingTasks.week05;
-
 public class FindTheUnique {
+    public static void main(String[] args) {
+        System.out.println("findTheUnique(\"AAABBBCCCDEF\") = " + findTheUnique("AAABBBCCCDEF"));
+    }
+    public static String findTheUnique(String str) {
+        String resultString = "";
+        for (char i = 0; i < str.length(); i++) {
+            if (!resultString.contains("" + str.charAt(i))) {
+                resultString += "" + str.charAt(i);
+            }
+        }
+        return resultString;
+    }
 }
 
 /*
