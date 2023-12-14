f9cb35410d243a6097256e4bd06183b9c445a3a9
She1ter <ievgen.p.tkachenko@gmail.com>
2023-12-13 21:58:31 -0600

    ArrayListSortingDesc created

---
+++ src/Eugene_codingTasks/week10/ArrayListSortingDesc.java
@@ -0,0 +1,24 @@
+package Eugene_codingTasks.week10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListSortingDesc {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(100, 2, 58, 67, 8, 65, 2, 41));
+        arrayListSortingInASC(arr);
+    }
+
+    public static void arrayListSortingInASC(ArrayList<Integer> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = 0; j < arr.size(); j++) {
+                if (arr.get(j) < arr.get(i)) { //checking if j<i move j back and j should start from index i
+                    int temp= arr.get(i);               //temporary store variable at index i
+                    arr.set(i, arr.get(j));             //moving variable at index j to index i
+                    arr.set(j,temp);                    //assigning temp variable to index j
+                }
+            }
+        }
+        System.out.println(arr);
+    }
+}
